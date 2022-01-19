3b7c11831f0733c7dfaeb06e8c27443eaeae3dc5
Kevin Morris <kevr@0cost.org>
2022-01-19 14:57:50 -0800

    fix(stubs/ncurses.cpp): preprocessor-swap initscr as well
    Signed-off-by: Kevin Morris <kevr@0cost.org>

--- src/stubs/ncurses.cpp
+++ src/stubs/ncurses.cpp
@@ -6,13 +6,15 @@ extern "C" {
 // Use some preprocessor hackery to avoid bringing in
 // the stub's real functions. We include ncurses.h here
 // to gain access to WINDOW for the stub functions.
+#define initscr _initscr
 #define wrefresh _wrefresh
-#define endwin _endwin
 #define delwin _delwin
+#define endwin _endwin
 #include <ncurses.h>
+#undef initscr
 #undef wrefresh
-#undef endwin
 #undef delwin
+#undef endwin
 
 //! noop
 WINDOW *initscr(void)
