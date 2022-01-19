9aaa57a40d3950bafdc6020e2122a8c534716100
Kevin Morris <kevr@0cost.org>
2022-01-19 09:58:55 -0800

    fix: space invalid arg output
    Signed-off-by: Kevin Morris <kevr@0cost.org>

--- src/main.cpp
+++ src/main.cpp
@@ -10,7 +10,7 @@
 int main(int argc, char **argv)
 {
     if (argc != 1) {
-        std::cout << argv[0] << "takes no arguments.\n";
+        std::cout << argv[0] << " takes no arguments.\n";
         return 1;
     }
     std::cout << "This is project " << PROJECT_NAME << ".\n";
