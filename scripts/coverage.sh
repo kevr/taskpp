4120f02cc674f9001d76dbdb3b556009704fc284
Kevin Morris <kevr@0cost.org>
2022-01-24 08:10:33 -0800

    meson: show-coverage -> cov, improve cov
    - renamed meson's show-coverage target to cov
    - added --xml flag to coverage.sh, runs coverage-xml
    - added logic to coverage.sh, runs coverage-text before outputting
    - simplified .gitlab-ci.yml
    
    This patch very much simplifies coverage targets through meson by
    enclosing coverage-producing targets within the coverage.sh script,
    which is called via the new cov target:
    
        $ ninja -C build test
        $ ninja -C build cov
    
    Note: Nothing is done for XML after running coverage-xml, CI
    needs to move the XML to the proper place post-execution.
    
    Signed-off-by: Kevin Morris <kevr@0cost.org>

--- scripts/coverage.sh
+++ scripts/coverage.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
+# Copyright (C) 2022 Kevin Morris
+# Complete GPLv2 text can be found in LICENSE.
 set -eou pipefail
+
+ninja coverage-text
+
+if [ $# -gt 0 ] && [ "$1" = "--xml" ]; then
+    ninja coverage-xml
+fi
+
 exec cat "${MESON_BUILD_ROOT}/meson-logs/coverage.txt"
